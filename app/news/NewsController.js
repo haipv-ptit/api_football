9c4e7b77c48397c74cd3dd1a9d3d26ec6701bae5
haipv-ptit <haipv.dev@gmail.com>
2018-07-05 22:43:34 +0700

    Repository content for api detail

--- app/news/NewsController.js
+++ app/news/NewsController.js
@@ -5,7 +5,6 @@ var repository = require('./NewsRepository');
 router.get('/get/:id', function(req, res, next) {
 	repository.getNewsDetail(req.params.id, (result)=> {
 		if(result) {
-			result['created_at'] = Date.parse(result['created_at'])/1000;
 			let data = {'data': result};
 			res.json(data);
 		} else {
--- app/news/NewsRepository.js
+++ app/news/NewsRepository.js
@@ -1,5 +1,25 @@
 const db = require('../../db');
 
+const REPOSITORY_TEMPLATE = '<html>'+
+'	<head>'+
+'		<meta charset="utf-8">'+
+'		<meta name="viewport" content="width=device-width, inital-scale=1">'+
+'		<style type="text/css">'+
+'			body {'+
+'				margin: 0 !important;'+
+'				padding: 0 !important;'+
+'				font-size: 20px;'+
+'			}'+
+'			img {'+
+'				max-width: 100% !important;'+
+'			}'+
+'		</style>'+
+'	</head>'+
+'	<body>'+
+'		{$content}'+
+'	</body>'+
+'</html>';
+
 module.exports = {
     getNewsDetail: (newsId, callback) => {
         let conn = db.getDb();
@@ -11,6 +31,11 @@ module.exports = {
                 let data = null;
                 if(results && results.length > 0) {
                     data = results[0];
+                    result['created_at'] = Date.parse(result['created_at'])/1000;
+                    let content = data['content'];
+                    if(content !== null && content !== "") {
+                        data['content'] = REPOSITORY_TEMPLATE.replace("{$content}", content);
+                    }
                 }
                 callback(data);
             }
